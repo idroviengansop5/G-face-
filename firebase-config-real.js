9696f4f33c1926e724b8281caeaa9c647fdf400d
idroviengansop5 <idroviengansop5@gmail.com>
2026-09-07 13:47:14 +0100

    Update Firebase configuration with new credentials

--- firebase-config-real.js
+++ firebase-config-real.js
@@ -1,15 +1,14 @@
 
 const firebaseConfig = {
-  apiKey: "G-face",
-  authDomain: "G-face.firebaseapp.com",
-  projectId: "G-face",
-  storageBucket: "G-face.appspot.com",
-  messagingSenderId: "G-face",
-  appId: "G-face"
+  apiKey: "AIzaSyBMYM1A7euaMxN45du8PAZa7nwq5ICLuog",
+  authDomain: "g-face-5e983.firebaseapp.com",
+  projectId: "g-face-5e983",
+  storageBucket: "g-face-5e983.firebasestorage.app",
+  messagingSenderId: "511144237117",
+  appId: "1:511144237117:web:5f50d90bf7fa705147b254"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
-const storage = firebase.storage();
